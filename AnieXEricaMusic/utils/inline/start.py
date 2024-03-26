a08b1063e6e972171ff9d67409305b1f455c5854
AshBorn <anishsingh0119@gmail.com>
2024-03-26 17:50:19 +0530

    Align Code

--- AnieXEricaMusic/utils/inline/start.py
+++ AnieXEricaMusic/utils/inline/start.py
@@ -1,21 +1,22 @@
 from pyrogram.types import InlineKeyboardButton
-
 import config
 from AnieXEricaMusic import app
-
 
 def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users+"
+                text=_["S_B_1"], 
+                url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users+"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text=_["S_B_2"], 
+                url=config.SUPPORT_CHAT
+            ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
-
 
 def private_panel(_):
     buttons = [
@@ -29,7 +30,6 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
         ],
         [InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG")],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
